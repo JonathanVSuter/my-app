f690e5ac8a1b4517c358c01132df94a8a6cfeb25
Jonathan Suter <jonathan.vinicius.suter@gmail.com>
2023-06-16 00:49:55 -0300

    .

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -4,6 +4,5 @@ function ChildComponent(props){
     return (<div>
         <button onClick={() => props.greetHandler('child')}>Greet Parent</button>
     </div>)
-
 }
 export default ChildComponent;
